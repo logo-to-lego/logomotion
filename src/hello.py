88f70a3bdc3dc9ba119dfe8f45cefc905ddb449b
J-Uhero <juho.herranen@helsinki.fi>
2022-09-12 16:52:34 +0300

    Modified hello.py and .coveragerc for testing CI environment

--- src/hello.py
+++ src/hello.py
@@ -1,5 +1,12 @@
+# pylint: disable=missing-module-docstring, too-few-public-methods
 class Hello:
     """Test class for testing CI environment"""
 
+    @staticmethod
     def hello_world(console_io):
+        """Hello world
+
+        Args:
+            console_io (IO objeck): object (mock) for IO operations
+        """
         console_io.write("hello world")
