191bd6dba5e776ca8c0003daed0a29114029563c
Veloxization <github@velo.anonaddy.com>
2022-09-27 10:54:06 +0300

    Added classes for semantic analysis error collection

---
+++ src/entities/error_values.py
@@ -0,0 +1,15 @@
+#pylint: disable=too-few-public-methods
+"""Module for the ErrorValue class"""
+
+class ErrorValues:
+    """Class to keep track error values to AST nodes"""
+    def __init__(self):
+        """Initialize a new ErrorValues object"""
+        self.errors = []
+
+    def add_error(self, error):
+        """Add a new error to the value list
+        Args:
+            error: The ErrorInfo object to add to the list"""
+
+        self.errors.append(error)
