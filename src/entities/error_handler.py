142cc280277ea671d958a9bc31a5e37b290d29ef
yuzamonkey <jusamankki@gmail.com>
2022-10-05 10:18:12 +0300

    refactor error_handler.py

--- src/entities/error_handler.py
+++ src/entities/error_handler.py
@@ -1,35 +1,34 @@
 import json
 import os
+import sys
 
 class ErrorHandler():
     
-    def __init__(self, language = "ENG"):
-        errors = []
+    def __init__(self, language = "FIN"):
+        self.errors = []
         self.language = language
-
-        self.parser_error_messages = self._get_parser_dict()
-        #print(self.parser_error_messages)
+        self.error_dir = os.path.join(os.path.dirname(os.path.relpath(__file__)), '../error_messages')
 
     def _get_parser_dict(self):
-        dir_path = os.path.join(os.path.dirname(os.path.relpath(__file__)), '../error_messages')
-        parser_errors_filepath = os.path.join(dir_path, 'parser_errors.json')
-        parser_errors_file = open(parser_errors_filepath)
+        parser_errors_file = open(os.path.join(self.error_dir, 'parser_errors.json'))
         return json.load(parser_errors_file)
 
-    def add_error(self, id: int, **kwargs):
-        try:
-            msg = self.parser_error_messages[str(id)][self.language]
-        except KeyError as ke:
-            print("Error id {id} could not be found from parser_errors.json".format(id=id))
-            return
+    def _get_message(self, id):
+        if 1000 <= id <= 1999:
+            pass
+        elif 2000 <= id <= 2999:
+            return self._get_parser_dict()[str(id)][self.language]
+        elif 3000 <= id <= 3999:
+            pass
+        else:
+            raise Exception("ID {id} was not found from error message files".format(id=id))
 
-        print(msg)
-        #msg = msg.format(row=kwargs["row"], column=kwargs["column"], prodval=kwargs["prodval"])
+    def add_error(self, id: int, **kwargs):
+        msg: str = self._get_message(id)
+      
         for key, value in kwargs.items():
             msg = msg.replace(f"@{key}", str(value))
-        print(msg)
-        #for key, value in kwargs.items():
-        #    print("KWARG ITEM", key, value)
+        print(msg + "\n")
 
 
 default_error_handler = ErrorHandler()
