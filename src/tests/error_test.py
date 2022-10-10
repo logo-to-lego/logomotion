9c99d86aef4f9b9060c7ea1fc554d414cd361923
yuzamonkey <jusamankki@gmail.com>
2022-10-10 13:53:45 +0300

    add binop check_types() error msg and test

--- src/tests/error_test.py
+++ src/tests/error_test.py
@@ -29,5 +29,22 @@ class TestErrorHandler(unittest.TestCase):
         )
         eng_expected_msg = "I could not understand 'mak'. I found this on row 1 and column 3."
 
+        self.assertEqual(len(self.error_handler.get_error_messages()), 1)
+        self.assertEqual(self.error_handler.get_error_messages()[0]["FIN"], fin_expected_msg)
+        self.assertEqual(self.error_handler.get_error_messages()[0]["ENG"], eng_expected_msg)
+
+    def test_error_with_invalid_binop(self):
+        test_string = """
+            make "a "kissa
+            make "b 1 + :a
+            """
+
+        ast = self.parser.parse(test_string)
+        ast.check_types()
+        
+        fin_expected_msg = "Rivillä 3 yritit tehdä laskutoimituksen jollakin joka ei ole numero."
+        eng_expected_msg = "In row 3 you tried to calculate with something that is not a number."
+
+        self.assertEqual(len(self.error_handler.get_error_messages()), 1)
         self.assertEqual(self.error_handler.get_error_messages()[0]["FIN"], fin_expected_msg)
         self.assertEqual(self.error_handler.get_error_messages()[0]["ENG"], eng_expected_msg)
