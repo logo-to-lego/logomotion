c8caf627df64d6d84f69550aeedf4ddb0d9598fb
TuuPu <puontit@gmail.com>
2022-10-18 10:31:37 +0300

    Clarified generate_code function with a comment

--- src/parser/ast.py
+++ src/parser/ast.py
@@ -126,6 +126,8 @@ class Move(Node):
             code = f"robot.travel(-{arg_var});"
         elif self.type == TokenType.LT:
             code = f"robot.rotate({arg_var});"
+        #Else refers to rotate RT. This has to be changed
+        #if other movement patterns are done.
         else:
             code = f"robot.rotate(-{arg_var});"
 
