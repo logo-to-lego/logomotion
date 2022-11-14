bfb1b1a09d0e1c552ad1f73540d2cd4a2d42c74d
yuzamonkey <jusamankki@gmail.com>
2022-11-14 14:49:37 +0200

    refactor symbol.py

--- src/entities/symbol.py
+++ src/entities/symbol.py
@@ -1,23 +1,9 @@
 """symbol module"""
 from entities.type import Type
 
-
-class Variable:
-    "Class for symbol in symbol table"
-
-    def __init__(self, name: str, typeclass=None):
-        """Constructor function for Variable object
-
-        Args:
-            name (str): Variables name
-            typeclass (Type, optional): Class instance of variable values type.
-                Defaults to new type class.
-        """
+class Symbol:
+    def __init__(self, name, typeclass):
         self._name = name
-        if not typeclass:
-            typeclass = Type(variables={name})
-        if not isinstance(typeclass, Type):
-            raise TypeError("Symbols type must be an instance of Type class")
         self._typeclass = typeclass
 
     @property
@@ -34,14 +20,32 @@ class Variable:
     def typeclass(self, typeclass: Type):
         self._typeclass = typeclass
 
-    def __str__(self):
-        return f"Variable {self._name}: Typeclass: ({self._typeclass})"
-
     def get_logotype(self):
         return self._typeclass.logotype
 
 
-class Function(Variable):
+class Variable(Symbol):
+    "Class for symbol in symbol table"
+
+    def __init__(self, name: str, typeclass=None):
+        """Constructor function for Variable object
+
+        Args:
+            name (str): Variables name
+            typeclass (Type, optional): Class instance of variable values type.
+                Defaults to new type class.
+        """
+        if not typeclass:
+            typeclass = Type(variables={name})
+        if not isinstance(typeclass, Type):
+            raise TypeError("Symbols type must be an instance of Type class")
+        super().__init__(name, typeclass)
+
+    def __str__(self):
+        return f"Variable {self._name}: Typeclass: ({self._typeclass})"
+
+
+class Function(Symbol):
     "Class for functions in symbol table. Inherits Variable class"
 
     def __init__(self, name, params=None, typeclass=None):
@@ -52,7 +56,6 @@ class Function(Variable):
             params ([Variable], optional): Arguments given to function. Defaults to None.
             typeclass (Type, optional): Functions return values type. Defaults to LogoType.UNKNOWN
         """
-        self._name = name
         if not params:
             self._parameters = []
         else:
@@ -61,15 +64,15 @@ class Function(Variable):
             typeclass = Type(functions={name})
         if not isinstance(typeclass, Type):
             raise TypeError("Symbols type must be an instance of Type class")
-        self._typeclass = typeclass
+        super().__init__(name, typeclass)
+
+    @property
+    def parameters(self):
+        """return functions arguments as list"""
+        return self._parameters
 
     def __str__(self):
         params_str = ""
         if self._parameters:
             params_str = f", parameters: {str(self._parameters)}"
         return f"Function {self._name}: typeclass: ({self._typeclass})" + params_str
-
-    @property
-    def parameters(self):
-        """return functions arguments as list"""
-        return self._parameters
