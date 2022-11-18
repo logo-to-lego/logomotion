d2c7bb3f2430a369c3f50fdb8feab75695377a18
haxsampo <sampo.hakola@helsinki.fi>
2022-11-18 13:20:14 +0200

    reverted a problem caused by black

--- src/utils/code_generator.py
+++ src/utils/code_generator.py
@@ -6,7 +6,8 @@ from utils.logger import Logger, default_logger
 from lexer.token_types import TokenType
 
 START_METHOD = (
-    "package logo; import classes.EV3MovePilot; import java.lang.Runnable;" "public class Logo { "
+    "package logo; import classes.EV3MovePilot; import java.lang.Runnable;"\
+        "public class Logo { "
 )
 START_MAIN = (
     "public static void main(String[] args) { "
