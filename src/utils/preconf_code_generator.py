bed521821b62eb26940ccf1b0959b48d922bbfbb
haxsampo <sampo.hakola@helsinki.fi>
2022-11-18 12:43:51 +0200

    small change to preconf func structure

--- src/utils/preconf_code_generator.py
+++ src/utils/preconf_code_generator.py
@@ -1,16 +1,14 @@
 class JavaPreconfFuncsGenerator:
     """A class for generating preconfigured functions Java code"""
     
-    def __init__(self):
-        self.java_preconf_funcs_dict = {"repeat": self.get_repeat_java()}
-    
-    def get_repeat_java(self):
-        repeat_code = "public static void repeat(int n, Runnable f) { \
-                    for(int i=0;i<2;i++) { \
-                        f.run(); \
-                    } \
+    java_repeat_code = "public static void repeat(int n, Runnable f) {\
+                    for(int i=0;i<n;i++) {\
+                        f.run();\
+                    }\
                   }"
-        return repeat_code
+    
+    def __init__(self):
+        self.java_preconf_funcs_dict = {"repeat": self.java_repeat_code}
     
     def get_funcs(self):
         return self.java_preconf_funcs_dict
