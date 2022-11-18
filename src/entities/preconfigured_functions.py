fc94d020affd1979f4cd9279328d444b4c2867b4
haxsampo <sampo.hakola@helsinki.fi>
2022-11-18 14:19:54 +0200

    for attempt:1

--- src/entities/preconfigured_functions.py
+++ src/entities/preconfigured_functions.py
@@ -11,6 +11,7 @@ def initialize_logo_functions(symbol_tables):
         symbol_tables: class SymbolTables
     """
     symbol_tables.functions.insert("repeat", _repeat())
+    symbol_tables.functions.insert("for", _for())
     return symbol_tables
 
 
@@ -19,12 +20,38 @@ def _repeat():
     Returns
         class Function
     """
+    # fname is the name that will be called from functions table
     fname = "repeat"
+    # initialize params as variables
     repeat_n = Variable("n", Type(logotype=LogoType.FLOAT))
     nameless = Variable("block", Type(logotype=LogoType.NAMELESS_FUNCTION))
+    # Initialize function, pass name and parans, define typeclass
+    # typeclasses logotype should reflect the return value
     procedure = Function(
         fname,
         params=[repeat_n, nameless],
         typeclass=Type(logotype=LogoType.VOID, functions=set((fname,))),
     )
     return procedure
+
+#Typical for loop. The controllist specifies three or four members: 
+# the local varname, start value, limit value, and optional step size
+def _for():
+    """Definition for 'for' nameless function
+    Returns
+    
+    """
+    fname = "for"
+    iterator_name = Variable("iterator_name", Type(logotype=LogoType.STRING))
+    start = Variable("start", Type(logotype=LogoType.FLOAT))
+    limit = Variable("limit", Type(logotype=LogoType.FLOAT))
+    stepsize = Variable("stepsize", Type(logotype=LogoType.FLOAT))
+    nameless = Variable("block", Type(logotype=LogoType.NAMELESS_FUNCTION))
+    procedure = Function(
+        fname,
+        params=[iterator_name, start, limit, stepsize, nameless],
+        typeclass=Type(logotype=LogoType.VOID, functions=set((fname,))),
+    )
+    return procedure
+    
+    
--- src/utils/preconf_code_generator.py
+++ src/utils/preconf_code_generator.py
@@ -6,6 +6,19 @@ class JavaPreconfFuncsGenerator:
                         f.run();\
                     }\
                   }"
+                  
+    java_for_code = "public static void repeat(String varname, int itr, int limit, Runnable f) {\
+        for(int varname=itr;limit<=itr;itr++) {\
+            f.run();\
+            }}\
+            \
+            public static void repeat(String varname, int itr, int limit, int stepsize, Runnable f) {\
+                for(int varname = itr ; itr <= limit ; itr += stepsize) {\
+                    f.run()\
+                    }}"
+                  
+    #Typical for loop. The controllist specifies three or four members: 
+    # the local varname, start value, limit value, and optional step size
 
     def __init__(self):
         self.java_preconf_funcs_dict = {"repeat": self.java_repeat_code}
